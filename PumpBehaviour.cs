01714ba2bd5a3d6679d7f67fd5b67610bfcc0b68
Subarked <68792391+Subarked@users.noreply.github.com>
2024-03-07 09:38:44 -0600

    Turn PumpBehaviour into a ToggleInteractableObject
    also reformatted some stuff and changed/added some comments

--- PumpBehaviour.cs
+++ PumpBehaviour.cs
@@ -1,46 +1,44 @@
 using Godot;
 using System;
 
-public partial class PumpBehaviour : Node2D
+public partial class PumpBehaviour : ToggleInteractableObject
 {
-	[Export]
-	public bool IsOpen = false;
 	[Export]
 	public float PumpWidth;
 	private RoomBehaviour Room;
-	private bool prevIsOpen = true;
 	private Sprite2D sprite;
 	float TransferSpeed;
 
-	
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		sprite = FindChild("Sprite2D") as Sprite2D;
 		Room = GetParent() as RoomBehaviour;
-		TransferSpeed = PumpWidth * 20f*5f; //these numbers are complete bullshit, I just thought they looked good
+		TransferSpeed = PumpWidth * 20f * 5f; //I swear I'll learn fluid dynamics soon please bear with me
+	}
+
+	public override int SwitchedState(Player player)
+	{
+		sprite.Frame = base.State ? 0 : 1;
+		////Expanded ternary (?:) operator
+		//if (State) //if it's open now, it means it was closed before so we...
+		//{
+		//	sprite.Frame = 0; //...change to the open frame
+		//} else { //if it's closed now, it means it was open before so we....
+		//	sprite.Frame = 1; //...change to the closed frame
+		//}
+		return 1;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		if (IsOpen)
+		if (State)
 		{
-			if (!prevIsOpen)
-			{
-				sprite.Frame = 0;
-			}
-			float difference = Room.FloodAmount; //Hangover from stealing the code from DoorBehaviour
-			float pressure = Mathf.Clamp(Room.Area/difference,0,1); //Completely bullshit pressure calc
-			float TransferAmount = Mathf.Clamp(difference, -TransferSpeed * (float)delta, TransferSpeed * (float)delta); //Amount to transfer from Room0 to Room1, negative if transferring from Room1 to Room0
-			Room.FloodAmount -= TransferAmount*pressure; //Change Room0 water amounts, Multiply by pressure to make it slower at lower amounts
+			//I swear I'll learn fluid dynamics soon please bear with me
+			float pressure = Mathf.Clamp(Room.Area / Room.FloodAmount, 0, 1); //Completely bullshit pressure calc
+			float TransferAmount = Mathf.Clamp(Room.FloodAmount, -TransferSpeed * (float)delta, TransferSpeed * (float)delta); //Amount to transfer from Room0 to NULL, negative if transferring from NULL to Room0
+			Room.FloodAmount -= TransferAmount * pressure; //Change Room0 water amounts, Multiply by pressure to make it slower at lower amounts
 		}
-		if (!IsOpen)
-		{
-			if (prevIsOpen)
-			{
-				sprite.Frame = 1;
-			}
-		}
-		prevIsOpen = IsOpen;
 	}
 }
