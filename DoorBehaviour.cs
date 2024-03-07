19a55343c07719fe1ed5852bce4bd08c3aae48b3
Subarked <68792391+Subarked@users.noreply.github.com>
2024-03-07 09:19:47 -0600

    Turn DoorBehaviour into a ToggleInteractableObject

--- DoorBehaviour.cs
+++ DoorBehaviour.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class DoorBehaviour : Node2D
+public partial class DoorBehaviour : ToggleInteractableObject
 {
 	[Export]
 	public RoomBehaviour Room0;
@@ -9,46 +9,43 @@ public partial class DoorBehaviour : Node2D
 	public RoomBehaviour Room1;
 	[Export]
 	public float DoorHeight;
-	[Export]
-	public bool IsOpen = false;
 	private bool prevIsOpen = false;
 	private StaticBody2D collider;
 	float TransferSpeed;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		TransferSpeed = DoorHeight * 20f*5f;
+		TransferSpeed = DoorHeight * 20f * 5f;
 		collider = FindChild("StaticBody2D") as StaticBody2D;
 	}
 
 	public override void _PhysicsProcess(double delta)
 	{
-		if (IsOpen)
+		if (State)
 		{
-			if (!prevIsOpen)
-			{
-				collider.ProcessMode = ProcessModeEnum.Disabled;
-			}
-
 			float difference = Room0.FloodAmount - Room1.FloodAmount; //Room0 > Room1 = +, Room0 < Room1 = -
-			float pressure = Mathf.Clamp(Room0.Area/difference/2f+Room1.Area/difference/2f,0,1); //Completely bullshit pressure calc XD
+			float pressure = Mathf.Clamp(Room0.Area / difference / 2f + Room1.Area / difference / 2f, 0, 1); //Completely bullshit pressure calc XD
 			float TransferAmount = Mathf.Clamp(difference, -TransferSpeed * (float)delta, TransferSpeed * (float)delta); //Amount to transfer from Room0 to Room1, negative if transferring from Room1 to Room0
-			Room0.FloodAmount -= TransferAmount*pressure; //Change Room0 water amounts, Multiply by pressure to make it slower at lower amounts
-			Room1.FloodAmount += TransferAmount*pressure; //Change Room1 water amounts, Multiply by pressure to make it slower at lower amounts
+			Room0.FloodAmount -= TransferAmount * pressure; //Change Room0 water amounts, Multiply by pressure to make it slower at lower amounts
+			Room1.FloodAmount += TransferAmount * pressure; //Change Room1 water amounts, Multiply by pressure to make it slower at lower amounts
 		}
-		if (!IsOpen)
-		{
-			if (prevIsOpen)
-			{
-				collider.ProcessMode = ProcessModeEnum.Inherit;
-			}
-		}
+	}
+	public override int SwitchedState(Player player)
+	{
 
-		prevIsOpen = IsOpen;
+		collider.ProcessMode = State ? ProcessModeEnum.Disabled : ProcessModeEnum.Inherit;
+		////Expanded ternary (?:) operator
+		//if (State) //if it's open now, it means it was closed before so we...
+		//{
+		//	collider.ProcessMode = ProcessModeEnum.Disabled; //...turn off the collision
+		//} else { //if it's closed now, it means it was open before so we....
+		//	collider.ProcessMode = ProcessModeEnum.Inherit; //...turn on the collision
+		//}
+		return 1;
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		Modulate = IsOpen ? new Color(.5f, .5f, .5f) : new Color(1f, 1f, 1f);
+		Modulate = State ? new Color(.5f, .5f, .5f) : new Color(1f, 1f, 1f);
 	}
 }
