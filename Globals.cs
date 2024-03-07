25aadd96ca4dfb63b7ec538f395b1e657c4b89ba
Subarked <68792391+Subarked@users.noreply.github.com>
2024-03-07 08:37:13 -0600

    Add Comment To Globals

--- Globals.cs
+++ Globals.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 
+//class being used as a way to store variables that should be open to every script in the game
 public class Globals
 {
 	public static Vector2 CameraCenter = new Vector2(200,150);
