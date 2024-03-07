0a43d00aaa9b488848cefbd10ad0ab10ee6b9c82
Subarked <68792391+Subarked@users.noreply.github.com>
2024-03-07 09:56:12 -0600

    Update CameraControl.cs

--- CameraControl.cs
+++ CameraControl.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class CameraControl : Camera2D
+public partial class CameraControl : Node2D
 {
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
