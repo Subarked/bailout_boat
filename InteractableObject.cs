6769a462b6d5225f125802bdb3959bfec9842641
Subarked <68792391+Subarked@users.noreply.github.com>
2024-03-07 09:31:19 -0600

    Add maxInteractDistance to InteractableObject
    there goes the meaning to the word "Interactable"

--- InteractableObject.cs
+++ InteractableObject.cs
@@ -3,7 +3,18 @@ using System;
 
 public partial class InteractableObject : Node2D
 {
-	public virtual int Interact(Player player) {
+	[Export]
+	public double maxInteractDistance = Math.Pow(Math.Pow(32, 2) + Math.Pow(16, 2), 1d / 2d);
+	public int Interact(Player player)
+	{
+		if (player.GlobalPosition.DistanceTo(this.GlobalPosition) <= maxInteractDistance)
+		{
+			return Interacted(player);
+		}
+		return 0;
+	}
+	public virtual int Interacted(Player player)
+	{
 		return 1;
 	}
 }
