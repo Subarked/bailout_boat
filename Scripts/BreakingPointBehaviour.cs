0facecc1faf35bb4b8c91bc4145702c493acff9e
Subarked <68792391+Subarked@users.noreply.github.com>
2024-03-13 12:40:55 -0500

    rename Breaking to BreakingPointBehaviour

--- Scripts/BreakingPointBehaviour.cs
+++ Scripts/BreakingPointBehaviour.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class Breaking : InteractableObject
 {
+	[Export]
+	public bool broken = false;
 	public double damage = 0;
 	private Sprite2D sprite2D;
 	// Called when the node enters the scene tree for the first time.
@@ -15,15 +17,19 @@ public partial class Breaking : InteractableObject
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
-		damage = Math.Clamp(damage+delta/60d,0d,1d);
-		sprite2D.Frame = (int)(damage*(sprite2D.Vframes*sprite2D.Hframes-1));
+		if (broken)
+		{
+			damage = Math.Clamp(damage + delta / 60d, 0d, 1d);
+			sprite2D.Frame = (int)(damage * (sprite2D.Vframes * sprite2D.Hframes - 1));
+		}
+
 	}
 
 	public override int Interacted(Player player)
 	{
 		GD.Print("aaaa!");
-		damage = Math.Clamp(damage-10d/60d,0,1);
+		damage = Math.Clamp(damage - 10d / 60d, 0d, 1d);
+		sprite2D.Frame = (int)(damage * (sprite2D.Vframes * sprite2D.Hframes - 1));
 		return 1;
 	}
 }
